67b6e1e60f23119b609fe26781d8606edb754606
peter-james-obrien <peter.o'brien@infotrack.com.au>
2019-11-15 15:59:12 +1100

    add basic persistence layer and in-memory test database

---
+++ Src/Persistence/BattleshipDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ofx.Battleship.Application.Common.Interfaces;
+using Ofx.Battleship.Domain.Entities;
+
+namespace Ofx.Battleship.Persistence
+{
+    public class BattleshipDbContext : DbContext, IBattleshipDbContext
+    {
+        public BattleshipDbContext(DbContextOptions<BattleshipDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Game> Games { get; set; }
+    }
+}
---
+++ Tests/Application.UnitTests/Common/BattleshipDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ofx.Battleship.Persistence;
+using System;
+
+namespace Application.UnitTests.Common
+{
+    public class BattleshipDbContextFactory
+    {
+        public static BattleshipDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<BattleshipDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new BattleshipDbContext(options);
+
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+    }
+}
