8406b730e807227457a7a9c02d663b9bd0f832f1
peter-james-obrien <peter.o'brien@infotrack.com.au>
2019-11-15 15:31:26 +1100

    update netstandard to 2.1 and fix build error

--- Src/Domain/Entities/ShipPart.cs
+++ Src/Domain/Entities/ShipPart.cs
@@ -5,7 +5,7 @@
         public int ShipPartId { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
-        public bool Hit { get; set; };
+        public bool Hit { get; set; }
 
         public Ship Ship { get; set; }
     }
