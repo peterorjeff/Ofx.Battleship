a9bc2ea41e5f10c676d2208851384b4de9725d57
peter-james-obrien <peter.o'brien@infotrack.com.au>
2019-11-16 14:15:02 +1100

    fixed in-memory database per scope issue

--- Src/Persistence/DependencyInjection.cs
+++ Src/Persistence/DependencyInjection.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ofx.Battleship.Application.Common.Interfaces;
-using System;
 
 namespace Ofx.Battleship.Persistence
 {
@@ -10,8 +8,10 @@ namespace Ofx.Battleship.Persistence
     {
         public static IServiceCollection AddPersistence(this IServiceCollection services)
         {
+            // Use In-Memory Database for simplicity here, can avoid standing up a db somewhere.
+            // Specify a constant name, so we get the same db across scoped lifetimes.
             services.AddDbContext<BattleshipDbContext>(options =>
-                options.UseInMemoryDatabase(Guid.NewGuid().ToString()));
+                options.UseInMemoryDatabase("BattleshipTestDatabase"));
 
             services.AddScoped<IBattleshipDbContext>(provider => provider.GetService<BattleshipDbContext>());
 
